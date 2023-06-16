b0b5c5d13d789fa15741b7a5aa609773d1da20b0
Anderson Amantino <88107035+anderson-mntn@users.noreply.github.com>
2023-06-16 17:15:35 -0300

    começando a criar as rotas com react-router-dom

--- src/App.js
+++ src/App.js
@@ -4,14 +4,53 @@ import Header from './components/Header'
 import Home from './components/Home';
 import Sobre from './components/Sobre';
 
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>
+  },
+  {
+    path: "/aulas",
+    element: <Aulas/>,
+  },
+  {
+    path: "/sobre",
+    element: <Sobre/>
+  },
+  {
+    path: "/aulas/a1",
+    element: <div>aula a1</div>
+  },
+]);
+
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Home/>
-      <Aulas/>
-      <Sobre/>
+    <Header/>
+    <RouterProvider router={router}></RouterProvider>
     </div>
+
+
+    // <Router>
+    //   <div className="App">
+    //     <Header/>
+
+    //     <Route path='/'>
+    //       <Home/>
+    //     </Route>
+
+    //     <Route path='/aulas'>
+    //       <Aulas/>
+    //     </Route>
+
+    //     <Route path='/sobre'>
+    //       <Sobre/>
+    //     </Route>
+        
+    //   </div>
+    // </Router>
   );
 }
 
