59cc1ff546cd75fe160142a98752c9ac5bbb893c
Anderson Amantino <88107035+anderson-mntn@users.noreply.github.com>
2023-06-17 23:10:27 -0300

    redicionamento para home após alguma ação

--- src/components/ContactDetails.js
+++ src/components/ContactDetails.js
@@ -1,12 +1,27 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
-const ContactDetails = () =>{
+function ContactDetails (props) {
+
+    // const msg = 'Voltando para pagina principal....';
+
+    // 6 - redirect - enviando o usuario para home (ou outro page) após concluir alguma ação
+    const navigate = useNavigate();
+
+    const handleContact = () => {
+        setTimeout(()=>{
+
+            //comunicação com o db
+            console.log("Mensagem enviada, voltando para pagina principal")
+            return navigate('/') // path para ser redirecionado
+        }, 4000)
+    }
 
     const { id } = useParams();
 
     return(<div>
+          
         <h2>Exibindo contato numero: {id}</h2>
+        <button onClick={handleContact}>Enviar Mensagem</button>
     </div>)
 }
 
