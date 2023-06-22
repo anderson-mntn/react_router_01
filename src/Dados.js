d1555ebd86a3454adf5d79a39ad98e73bed022c4
Anderson Amantino <88107035+anderson-mntn@users.noreply.github.com>
2023-06-21 21:53:31 -0300

    basic data adjust

--- src/Dados.js
+++ src/Dados.js
@@ -2,8 +2,8 @@ const dados = [
     {id: 0, title: "Aulas iniciais", desc: "Aulas introdutórias de algum assunto"},
     {id: 1, title: "Introdução a nossa plataforma", desc: "Descrição da introdução a plataforma"},
     {id: 2, title: "Aulas de Português", desc: "Descrição das aulas de português"},
-    {id: 2, title: "Aulas de Matematica", desc: "Descrição das aulas de matemática"},
-    {id: 2, title: "Aulas de Inglês", desc: "Descrição das aulas de inglês"},
+    {id: 3, title: "Aulas de Matemática", desc: "Descrição das aulas de matemática"},
+    {id: 4, title: "Aulas de Inglês", desc: "Descrição das aulas de inglês"},
 ]
 
 export default dados;
