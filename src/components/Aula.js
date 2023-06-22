01497b1a603a8eed465c5cb651fde26b8a5835f1
Anderson Amantino <88107035+anderson-mntn@users.noreply.github.com>
2023-06-21 22:04:30 -0300

    add: useState na const "aula", melhorando o código

--- src/components/Aula.js
+++ src/components/Aula.js
@@ -1,28 +1,20 @@
-import React, { useEffect } from 'react'
-import { useLocation, useMatch } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { useMatch } from 'react-router-dom'
 import dados from '../Dados';
-import { type } from '@testing-library/user-event/dist/type';
 
 export default function Aulas(){
 
-    // const p = useLocation();
     const match = useMatch('/aulas/:id');
     const params = match.params.id;
-    const aula = dados.filter(aula => aula.id == params)[0]
 
-   
+    const [aula, setAula] = useState({})
     useEffect(()=>{
-        // console.log(p)
-        //console.log(typeof(idDadoAula),"///", typeof(dados[0].id))
-        console.log(aula.desc)
-
-    })
-
-
+        setAula(dados.filter(aula => aula.id == params)[0])
+    }, [params])  
+  
+    
     return(<div className="page-aulas">
        <h2>{aula.title}</h2>
        <p>{aula.desc}</p>
-       
-
     </div>)
 }
