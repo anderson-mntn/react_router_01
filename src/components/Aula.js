1a20342f87c026c194563c957e39407fa231f181
Anderson Amantino <88107035+anderson-mntn@users.noreply.github.com>
2023-06-22 15:19:54 -0300

    transferindo usando Custom Hook

--- src/components/Aula.js
+++ src/components/Aula.js
@@ -1,18 +1,24 @@
 import React, { useEffect, useState } from 'react'
-import { useMatch } from 'react-router-dom'
+import { useMatch, useParams } from 'react-router-dom'
 import dados from '../Dados';
+
+function useAula(){
+    const id = useParams().id;
+    return dados.filter(aula => aula.id == id)[0]
+}
 
 export default function Aulas(){
 
-    const match = useMatch('/aulas/:id');
-    const params = match.params.id;
+    // const match = useMatch('/aulas/:id');
+    // const params = match.params.id;
 
-    const [aula, setAula] = useState({})
-    useEffect(()=>{
-        setAula(dados.filter(aula => aula.id == params)[0])
-    }, [params])  
-  
+    //const [aula, setAula] = useState({})
+    // useEffect(()=>{
+    //     setAula(dados.filter(aula => aula.id == params)[0])
+    // }, [params])  
     
+    const aula = useAula();
+
     return(<div className="page-aulas">
        <h2>{aula.title}</h2>
        <p>{aula.desc}</p>
