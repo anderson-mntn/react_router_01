bf19a8751a824f08dfdf45b5d2be9466a6791df8
Anderson Amantino <88107035+anderson-mntn@users.noreply.github.com>
2023-06-22 15:38:49 -0300

    1. useLocation e query - pegando o valor usando .get

--- src/components/Assistir.js
+++ src/components/Assistir.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+function useQuery(){
+    return new URLSearchParams(useLocation().search)
+}
 
 export default function Assistir(){
+
+    const query = useQuery()
+
+    useEffect(()=>{
+        console.log(query.get('v'))
+        // dentro de <prototype> terá métodos que podemos usar
+    })
+
     return(<div className="page">
         query é o que vem depois do interrogação
     </div>)
